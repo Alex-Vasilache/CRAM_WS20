48cc691e0f54aea4996496145492235db85c6559
Alex <alex.vasilache9@gmail.com>
2021-01-27 02:22:23 +0200

    Solved task2

--- Second run/Nr_2_SingleIndexModel/ps2_checker.py
+++ Second run/Nr_2_SingleIndexModel/ps2_checker.py
@@ -11,13 +11,13 @@
 ########### Task B ###########
 
 # B.3 How much variance of Adidas AG's return is explained by variations in systematic risk?
-b_3 = 0.00
+b_3 = 0.31
 
 # B.4 How much of return variance of Volkswagen AG is Volkswagen AG specific?
-b_4 = 0.00
+b_4 = 0.79
 
 # B.5 Statistical correlation between Adidas' and Volkswagens firm specific innovations
-b_5 = 0.00
+b_5 = 0.05
 
 # B.6 Average correlation of Daimler AG with the other ES50 constitutents
-b_6 = 0.00
+b_6 = 0.01
