33d59f679b9b66ba392f1ca40cdabe17486579c8
Alex <alex.vasilache9@gmail.com>
2021-01-28 00:19:23 +0200

    Solved Task 3 & 4

--- Second run/Nr_3_Vol (squared Returns)/ps3_checker.py
+++ Second run/Nr_3_Vol (squared Returns)/ps3_checker.py
@@ -8,61 +8,61 @@
 
 ######### Section E #########
 
-e_q1 = ''
-e_q2 = ''
-e_q3 = ''
-e_q4 = ''
+e_q1 = 'time-varying'
+e_q2 = 'no'
+e_q3 = 'no'
+e_q4 = 'yes'
 
 ######### Section G #########
 
-g1_q1 = ''
-g1_q2 = ''
-g1_q3 = ''
-g1_q4 = ''
+g1_q1 = 'yes'
+g1_q2 = 'yes'
+g1_q3 = 'yes'
+g1_q4 = 'yes'
 
 ######### Section H #########
 
-h_q1 = ''
+h_q1 = 'yes'
 
 ######### Section I #########
 
-i1_pc1_explained_variance = 0.0
-i1_pc2_explained_variance = 0.0
-i1_pc3_explained_variance = 0.0
-i1_pc4_explained_variance = 0.0
+i1_pc1_explained_variance = 0.42
+i1_pc2_explained_variance = 0.17
+i1_pc3_explained_variance = 0.11
+i1_pc4_explained_variance = 0.04
 
-i3_q1 = ''
-i3_q2 = ''
-i3_q3 = ''
-i3_q4 = ''
+i3_q1 = 'no'
+i3_q2 = 'yes'
+i3_q3 = 'unscaled data set'
+i3_q4 = 'normalize data'
 
-i4_corr_pc1_vow = 0.0
-i4_corr_pc1_market = 0.0
+i4_corr_pc1_vow = 1.0
+i4_corr_pc1_market = 0.11
 
 ######### Section J #########
 
-j_corr_pc1_market = 0.0
-j_q2 = ''
+j_corr_pc1_market = 0.95
+j_q2 = 'no'
 
 ######### Section K #########
 
-k_q1 = ''
-k_q2 = ''
-k_q3 = ''
-k_q4 = ''
-k_q5 = ''
-k_q6 = ''
+k_q1 = 'can'
+k_q2 = 'should'
+k_q3 = 'one useful'
+k_q4 = 'do'
+k_q5 = 'strong'
+k_q6 = '70%'
 
 ######### Section L #########
 
-l1_skew = 0.0
-l1_kurt = 0.0
+l1_skew = 17.6
+l1_kurt = 598.91
 
-l2_q1 = ''
-l2_q2 = ''
+l2_q1 = 'upward jumps'
+l2_q2 = 'vol in vol'
 
 ######### Section M #########
 
-m_q1 = ''
-m_q2 = ''
-m_q3 = ''
+m_q1 = 'no'
+m_q2 = 'pessimistic'
+m_q3 = 'yes'
