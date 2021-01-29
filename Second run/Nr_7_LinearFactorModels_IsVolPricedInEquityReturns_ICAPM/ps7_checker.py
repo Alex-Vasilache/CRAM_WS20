9d73fe1c485c64b1b08b3a852ff5463c9e252ebd
Alex <alex.vasilache9@gmail.com>
2021-01-29 12:29:56 +0200

    Task 7

--- Second run/Nr_7_LinearFactorModels_IsVolPricedInEquityReturns_ICAPM/ps7_checker.py
+++ Second run/Nr_7_LinearFactorModels_IsVolPricedInEquityReturns_ICAPM/ps7_checker.py
@@ -12,16 +12,16 @@
 ########### Task ###########
 
 # A.1 How important is each of the first five principal components?
-a1_pc1 = 0.00
-a1_pc2 = 0.00
-a1_pc3 = 0.00
-a1_pc4 = 0.00
-a1_pc5 = 0.00
+a1_pc1 = 0.69
+a1_pc2 = 0.15
+a1_pc3 = 0.06
+a1_pc4 = 0.05
+a1_pc5 = 0.03
 
 # A.2 Which of the FF7 variances is the main driver of PC1(z(Vol(FF7))?
 # ['MKT'/'SMB'/'HML'/'RMW'/'CMA'/'REV'/'MOM'/'cannot tell without further orthogonalization']
-a2 = ''
+a2 = 'cannot tell without further orthogonalization'
 
 # B.1 Are innovations to FF7 volatility priced in the cross-section of US equity returns?
 # ['yes'/'no']
-b1 = ''
+b1 = 'no'
